d7aecbbf6c5554db8499df0cff8a9a5e78275bd1
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-10-18 00:46:54 +0100

    Basic javascript functionality to add tasks

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 // Selects the input field (text-field)
-var submitButton = document.getElementById("submit-btn");
+var taskInput = document.getElementById("text-field");
 
 // select the button (submit-btn)
 var submitButton = document.getElementById("submit-btn");
 
 // select the list (task-list)
 var taskList = document.getElementById("task-list");
+
+// Attach event listener with addTask function to the button
+submitButton.addEventListener("click", addTask);
+
+// define function for addTask
+function addTask() {
+    var taskText = taskInput.value.trim(); // gets value and gets rid of spaces
+
+    if (taskText !== "") {
+
+        // Creates a new li 
+        var newListItem = document.createElement("li");
+
+        // Add the li to the task-list ul
+        newListItem.textContent = taskText;
+
+        taskList.appendChild(newListItem); 
+
+        taskInput.value = "";
+
+    } else {
+        alert("You can't enter an empty task! \nPlease type one in.");  
+    }
+}
 
