a66ebefc7fac48933922ab82337d1724fe7ba845
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-10-17 23:56:32 +0100

    Setup initial HTML & Javascript files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Selects the input field (text-field)
+var submitButton = document.getElementById("submit-btn");
+
+// select the button (submit-btn)
+var submitButton = document.getElementById("submit-btn");
+
+// select the list (task-list)
+var taskList = document.getElementById("task-list");
+
